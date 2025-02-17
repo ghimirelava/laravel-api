2c77edaaab01287643f8e0602ce3726c8d1f6a9a
ghimirelava <lghimire@usfca.edu>
2025-02-17 15:20:54 -0800

    generating fake data for testing

--- laravel-api/database/factories/InvoiceFactory.php
+++ laravel-api/database/factories/InvoiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,17 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        // Define randon fake data for status column
+        $status = $this->faker->randomElement(['billed', 'paid', 'void']);
+
         return [
-            //
+            'customer_id' => Customer::factory(), // customer_id comes from customer factory because its the foreign key
+            'amount' => $this->faker->numberBetween(100, 20000), // not 0 and not negative
+            'status' => $status,
+            'bill_date' => $this->faker->dateTimeThisDecade(),
+            // If status is paid then create a random date for paid_date column otherwise set it to null
+            'paid_date' => $status === 'paid' ? $this->faker->dateTimeThisDecade() : NULL,
+            // Ignore timestamps and table id as it is auto generated/incremented
         ];
     }
 }
