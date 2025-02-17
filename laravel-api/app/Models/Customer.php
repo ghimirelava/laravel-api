4d6001d73b12cba7d5595d5d458bc1a7e9985b10
ghimirelava <lghimire@usfca.edu>
2025-02-17 14:04:14 -0800

    Specifying a relationship between the Invoice and Customer models

---
+++ laravel-api/app/Models/Customer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory;
+
+    // Define the relationship between the Customer and Invoice models (1:M)
+    public function invoice() {
+        return $this->hasMany(Invoice::class);
+    }
+        
+}
