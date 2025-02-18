d64c92e8f1e1e2808d7b8b7b516d929d2bb057a5
ghimirelava <lghimire@usfca.edu>
2025-02-18 07:26:42 -0800

    enhance CustomerSeeder to generate diverse customer data with varying invoice counts

--- laravel-api/database/seeders/CustomerSeeder.php
+++ laravel-api/database/seeders/CustomerSeeder.php
@@ -5,13 +5,38 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// PHP class that allows you to populate the database with test data
 class CustomerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+
+    // Creating 230 customers each with diffrent number of invoices
     public function run(): void
     {
-        //
+        // 25 customers with 10 invoices each
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+        
+        // 100 customers with 10 invoices each
+        Customer::factory()
+            ->count(100)
+            ->hasInvoices(10)
+            ->create();
+
+        // 100 customers with 3 invoices each
+        Customer::factory()
+            ->count(100)
+            ->hasInvoices(3)
+            ->create();
+        
+        // 5 customers with no invoices
+        Customer::factory()
+            ->count(5)
+            ->create();
+            
     }
 }
